8c49766f21de47316115d7f63973ebb710ed43c4
NikosTsigkros <tsigkrosn@gmail.com>
2020-02-09 01:27:32 +0200

    modify face, edit function system

--- main.py
+++ main.py
@@ -6,39 +6,59 @@ SCREEN_HEIGHT = 800
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "pyGame")
 arcade.set_background_color(arcade.color.BLACK)
 
-# reset the screen - start draw -----------------------------
-arcade.start_render()
 
-# BODY
-ballX = SCREEN_WIDTH/2
-ballY = SCREEN_HEIGHT/2
-ballSize = SCREEN_WIDTH * 2/10
-arcade.draw_circle_filled(ballX, ballY, ballSize, arcade.color.YELLOW)
+def draw_face(mainX, mainY):
+    # BODY
+    ballX = mainX
+    ballY = mainY
+    ballSize = SCREEN_WIDTH * (2/10)
+    arcade.draw_circle_filled(ballX, ballY, ballSize, arcade.color.YELLOW)
 
-# LEFT EYE
-eyeX = ballX - (1/10) * ballX
-eyeY = ballY + (1/10) * ballX
-eyeSize = ballSize * 2/10
-arcade.draw_circle_filled(eyeX, eyeY, eyeSize, arcade.color.BLACK)
+    # LEFT EYE
+    eyeX = ballX - ballX * (1/10)
+    eyeY = ballY + ballY * (1/10)
+    eyeSize = ballSize * (2/10)
+    arcade.draw_circle_filled(eyeX, eyeY, eyeSize, arcade.color.BLACK)
 
-# RIGHT EYE
-eyeX = ballX + (1/10) * ballX
-eyeY = ballY + (1/10) * ballX
-eyeSize = ballSize * 2/10
-arcade.draw_circle_filled(eyeX, eyeY, eyeSize, arcade.color.BLACK)
+    # LEFT EYE (small)
+    eye2X = eyeX + eyeX * (3/100)
+    eye2Y = eyeY
+    eye2Size = eyeSize * (1/3)
+    arcade.draw_circle_filled(eye2X, eye2Y, eye2Size, arcade.color.WHITE)
 
-# MOUTH
-mouthX = ballX
-mouthY = ballY - 20
-mouthWidth = 30
-mouthHeight = 20
-startMouth = 190
-endMouth = 350
-arcade.draw_arc_filled(mouthX, mouthY, mouthWidth,
-                       mouthHeight, arcade.color.RED, startMouth, endMouth, 10)
+    # RIGHT EYE
+    eyeX = ballX + ballX * (1/10)
+    eyeY = ballY + ballY * (1/10)
+    eyeSize = ballSize * (2/10)
+    arcade.draw_circle_filled(eyeX, eyeY, eyeSize, arcade.color.BLACK)
 
-# end draw --------------------------------------------------
-arcade.finish_render()
+    # RIGHT EYE (small)
+    eye2X = eyeX + eyeX * (3/100)
+    eye2Y = eyeY
+    eye2Size = eyeSize * (1/3)
+    arcade.draw_circle_filled(eye2X, eye2Y, eye2Size, arcade.color.WHITE)
 
-# run
-arcade.run()
+    # MOUTH
+    mouthX = ballX
+    mouthY = ballY - (1/10) * ballY
+    mouthWidth = ballSize + ballSize * (1/10)
+    mouthHeight = mouthWidth * (1/3)
+    arcade.draw_ellipse_filled(mouthX, mouthY, mouthWidth,
+                               mouthHeight, arcade.color.RED)
+
+
+def main():
+    arcade.start_render()
+    # reset the screen - start draw -----------------------------
+
+    draw_face(50, 50)
+
+    # end draw --------------------------------------------------
+    arcade.finish_render()
+
+    # run
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
